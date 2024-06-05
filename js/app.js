8d38178210d81bc8881869df73e10a0c790c0aa1
AbdelRahman-Alwabarneh <abdelRahmanalwabarneh@gmail.com>
2024-06-05 20:35:09 +0300

    index.html

---
+++ js/app.js
@@ -0,0 +1,32 @@
+// let EnterNumber = prompt("Enter the number:");
+
+// function num(EnterNumber) {
+//   let x = EnterNumber.split("").reverse().join("");
+//   return x;
+// }
+// alert(num(EnterNumber));
+
+// function ENumber() {
+//   for (let i = 0; i <= 15; i++) {
+//     if (i % 2 == 0) {
+//       console.log(i + "is even");
+//     } else {
+//       console.log(i + "is odd");
+//     }
+//   }
+// }
+
+// ENumber();
+
+// let Number1 = prompt("Enter the word");
+// let array = Number1.split("");
+
+// function ENumber() {
+//   let arr = array.sort();
+//   return arr;
+// }
+// alert(ENumber());
+
+// let num = prompt("Enter the number:");
+// let array = num.split("");
+// for (let i=0; )
